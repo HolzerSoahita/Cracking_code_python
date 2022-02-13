01dacfce0194a5f05e56ea5a25c823b1728f0c06
Holzer <holzersoahita@gmail.com>
2022-02-13 19:54:49 +0300

    Transposition hacking and detect english module

---
+++ Transposition/transpositionHacker.py
@@ -0,0 +1,46 @@
+# Transposition Cipher Hacker
+
+import detectEnglish
+from transposition_encrypt_decrypt import decryptMessage
+
+
+def TranspositionHacking():
+    myMessage = """AaKoosoeDe5 b5sn ma reno ora'lhlrrceey e  enlh na  indeit n uhoretrm au ieu v er Ne2 gmanw,forwnlbsya apor tE.no euarisfatt  e mealefedhsppmgAnlnoe(c -or)alat r lw o eb  nglom,Ain one dtes ilhetcdba. t tg eturmudg,tfl1e1 v  nitiaicynhrCsaemie-sp ncgHt nie cetrgmnoa yc r,ieaa  toesa- e a0m82e1w shcnth  ekh gaecnpeutaaieetgn iodhso d ro hAe snrsfcegrt NCsLc b17m8aEheideikfr aBercaeu thllnrshicwsg etriebruaisss  d iorr."""
+    hackedMessage = hackTransposition(myMessage)
+
+    if hackedMessage == None:
+        print('Failed to hack encryption.')
+    else:
+        print('Copying hacked message to clipboard:')
+        print(hackedMessage)
+
+
+def hackTransposition(message):
+    print('Hacking...')
+
+    # Python programs can be stopped at any time by pressing Ctrl-C (on
+    # Windows) or Ctrl-D (on Mac and Linux)
+    print('(Press Ctrl-C or Ctrl-D to quit at any time.)')
+
+    # Brute-force by looping through every possible key.
+    for key in range(1, len(message)):
+        print('Trying key #%s...' % (key))
+        decryptedText = decryptMessage(key, message)
+
+        if detectEnglish.isEnglish(decryptedText):
+            # Ask user if this is the correct decryption.
+            print()
+            print('Possible encryption hack:')
+            print('Key %s: %s' % (key, decryptedText[:100]))
+            print()
+            print('Enter D if done, anything else to continue hacking:')
+            response = input('> ')
+
+            if response.strip().upper().startswith('D'):
+                return decryptedText
+
+    return None
+
+
+if __name__ == '__main__':
+    TranspositionHacking()
